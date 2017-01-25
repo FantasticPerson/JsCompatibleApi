1058e9682e192c8b28001ec33c61b1489a584509
dandan.wu <1240172019@qq.com>
2017-01-25 10:11:11 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * Created by wdd on 2017/1/25.
+ */
+export function findItem(arr,prop,value){
+    if(arr && arr.length > 0) {
+        if (arr.find) {
+            return arr.find(function (item) {
+                return item[prop] == value;
+            })
+        } else {
+            for (let i = 0; i < arr.length; i++) {
+                if (arr[i][prop] == value) {
+                    return arr[i];
+                }
+            }
+        }
+    }
+}
+
+export function myAssign(obj,propObj){
+    if(obj.assign){
+        Object.assign(obj,propObj);
+    } else {
+        let propNamesArray = Object.getOwnPropertyNames(propObj);
+        for(let i=0;i<propNamesArray.length;i++){
+            obj[propNamesArray[i]] = propObj[propNamesArray[i]];
+        }
+    }
+}
+
+export function getStrRepeat(str,times){
+    let result = '';
+    for(let i = 0;i<times;i++){
+        result += str;
+    }
+    return result;
+}
+
+export function myOnResize(func){
+    window.onresize = func;
+    document.body.onresize = func;
+}
